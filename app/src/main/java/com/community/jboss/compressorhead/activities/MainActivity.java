0d8c3464f6b263d98f7bfbad0fb3898d0bcb3970
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-12-07 02:26:11 +0800

    Initial UI pass (#68)
    Contains 3 main elements:
     * An "Info" MenuItem that displays usage instructions in a dialog
     * An ImageView for previewing the compressed image
     * A CardView containing text fields for each of the parameters,
       as well as 2 buttons - a 'Preview' button and a 'Download' button
    
    The functions of the UI elements should be pretty self-explanatory.
    
    In addition, fix the handling of strings slightly. Strings that
    should not be translatable are now marked as untranslatable.

--- app/src/main/java/com/community/jboss/compressorhead/activities/MainActivity.java
+++ app/src/main/java/com/community/jboss/compressorhead/activities/MainActivity.java
@@ -1,7 +1,14 @@
 package com.community.jboss.compressorhead.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.community.jboss.compressorhead.R;
 
@@ -11,5 +18,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_menu_info:
+                final InfoDialogFragment dialog = new InfoDialogFragment();
+                dialog.show(getSupportFragmentManager(), "info_dialog");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public static class InfoDialogFragment extends AppCompatDialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(R.string.title_info);
+            builder.setMessage(R.string.description_info_dialog);
+            builder.setPositiveButton(R.string.button_info_dialog, null);
+            return builder.create();
+        }
     }
 }
